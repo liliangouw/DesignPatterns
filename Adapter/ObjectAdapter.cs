56b7e135b938b6e62df6792eb0b8434c6ee94a10
liliangouw <1803443590@qq.com>
2022-12-26 22:44:17 +0800

    完成适配器模式

---
+++ Adapter/ObjectAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class ObjectAdapter : Target
+    {
+        private ObjectAdaptee adaptee;
+        public ObjectAdapter(ObjectAdaptee objectAdaptee) 
+        {
+            adaptee = objectAdaptee;
+        }
+        
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectAdaptee objectAdaptee = new ObjectAdaptee();
+            ObjectAdapter adapter = new ObjectAdapter(objectAdaptee);
+            adapter.Request();
+            Console.ReadKey();
+        }
+    }
+}
