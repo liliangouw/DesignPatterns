1e613cf373eea2cf6844e45f5acaeb23455647a1
liliangouw <1803443590@qq.com>
2023-01-02 22:50:21 +0800

    实现迭代器模式

---
+++ Iterator/ConcreteAggregate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    internal class ConcreteAggregate : Aggregate
+    {
+        private IList<object>Items= new List<object>();
+        public override Iterator GetIterator()
+        {
+           return new ConcreteIterator(this);
+        }
+        public int Count
+        {
+            get { return Items.Count; }
+        }
+        public object this[int index]
+        {
+            get { return Items[index]; }
+            set { Items.Insert(index, value); } 
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Iterator
+{
+    /// <summary>
+    /// 迭代器模式
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAggregate aggregate= new ConcreteAggregate();
+            aggregate[0] = "对象0";
+            aggregate[1] = "对象1";
+            aggregate[2] = "对象2";
+            aggregate[3] = "对象3";
+            aggregate[4] = "对象4";
+            aggregate[5] = "对象5";
+            aggregate[6] = "对象6";
+            Iterator iterator = new ConcreteIterator(aggregate);
+            object item=iterator.First();
+            while(!iterator.IsDone())
+            {
+                Console.WriteLine(iterator.CurrentItem());
+                iterator.Next();
+            }
+            Console.ReadKey();
+        }
+    }
+}
