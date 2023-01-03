6e77b8791301e4be2a6f998e5c8045283aa477ac
liliangouw <1803443590@qq.com>
2023-01-03 17:54:15 +0800

    添加访问者模式，待实现

---
+++ Visitor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Visitor
+{
+    /// <summary>
+    /// 访问者模式
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
