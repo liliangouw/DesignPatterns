ae54e20947c4dd3386cde7ab6b90a911fe7b128f
liliangouw <1803443590@qq.com>
2023-01-03 15:43:05 +0800

    完成状态模式

---
+++ State/Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Context
+    {
+        //赋初始状态
+        private State _state;
+        public Context(State state)
+        {
+            _state = state;
+        }
+        public State State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+                Console.WriteLine("当前的状态是{0}", _state.GetType().Name);
+            }
+        }
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
